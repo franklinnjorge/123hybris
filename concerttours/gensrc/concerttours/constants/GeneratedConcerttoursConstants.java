d6f08436a5553b2677852ec998fa58630fe1e3f5
Franklin Jorge <franklin-jorge@hotmail.com>
2019-12-10 10:56:22 -0300

    Add my new Extension

---
+++ concerttours/gensrc/concerttours/constants/GeneratedConcerttoursConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 10/12/2019 11:51:19                         ---
+ * ----------------------------------------------------------------
+ */
+package concerttours.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedConcerttoursConstants
+{
+	public static final String EXTENSIONNAME = "concerttours";
+	
+	protected GeneratedConcerttoursConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
